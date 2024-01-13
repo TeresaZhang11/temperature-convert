26bb2844e622023cb92da44c689944c43e05ee05
TeresaZhang11 <lingyiz4@illinois.edu>
2024-01-13 11:34:54 -0600

    added unit options

--- script.js
+++ script.js
@@ -1,12 +1,26 @@
-let celsius = document.getElementById('celsius');
-let fahrenheit = document.getElementById('fahrenheit');
+let input = document.getElementById('input');
+let inputUnit = document.getElementById('inputUnit');
 
-celsius.oninput = () => {
-    let output = parseFloat(celsius.value) * 9 /5 + 32 ;
-    fahrenheit.value = parseFloat(output.toFixed(2));
-};
+let output = document.getElementById('output');
+let outputUnit = document.getElementById('outputUnit');
 
-fahrenheit.oninput = () => {
-    let output = (parseFloat(fahrenheit.value) - 32) * 5 / 9;
-    celsius.value = parseFloat(output.toFixed(2));
+
+input.oninput = () => {
+
+    if(inputUnit.value === outputUnit.value){
+        output.value = parseFloat(input.value);
+    } else if (inputUnit.value === "celsius" && outputUnit.value === "fahrenheit"){
+        output.value = parseFloat((9 / 5 * input.value + 32).toFixed(2));
+    } else if (inputUnit.value === "celsius" && outputUnit.value === "kelvin"){
+        output.value = parseFloat((input.value + 273.15).toFixed(2));
+    } else if(inputUnit.value === "fahrenheit" && outputUnit.value === "celsius"){
+        output.value = parseFloat(((input.value - 32) * (5/9)).toFixed(2));
+    } else if(inputUnit.value === "fahrenheit" && outputUnit.value === "kelvin"){
+        output.value = parseFloat((5/9 *(input.value - 32) + 273.15).toFixed(2));
+    } else if(inputUnit.value === "kelvin" && outputUnit.value === "fahrenheit"){
+        output.value = parseFloat((9 / 5 * (input.value - 273.15) + 32).toFixed(2));
+    } else if(inputUnit.value === "kelvin" && outputUnit.value === "celsius"){
+        output.value = parseFloat((input.value - 273.15).toFixed(2));
+    }
+
 };
